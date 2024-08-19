261088cc56477e1987cf0826eb49624e84c124a6
alfonso lara <orozcoenrique159@gmail.com>
2024-08-19 17:46:30 -0500

    segundo commit

--- src/netgit/NetGit.java
+++ src/netgit/NetGit.java
@@ -4,6 +4,8 @@ public class NetGit {
 
     public static void main(String[] args) {
         System.out.println("Esto es una prueba, linea 1");
+        System.out.println("Hola a todos!");
+        
     }
     
 }
