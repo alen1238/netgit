f25c565f6d457cd22b29141d313b4a7210180980
alfonso lara <orozcoenrique159@gmail.com>
2024-08-19 17:51:30 -0500

    primer commit en rama testing

--- src/netgit/NetGit.java
+++ src/netgit/NetGit.java
@@ -5,7 +5,7 @@ public class NetGit {
     public static void main(String[] args) {
         System.out.println("Esto es una prueba, linea 1");
         System.out.println("Hola a todos!");
-        
+        System.out.println("Desde rama testing");
     }
     
 }
